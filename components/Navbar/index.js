65c29db2bd41f9535d0d25d4cd3a85cb0a259218
AnfalHussain <anfalae95@gmail.com>
2020-10-01 22:35:00 +0300

    navbar style

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Link from "next/link";
+
+//Material-ui
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
@@ -9,7 +13,6 @@ import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import Slide from "@material-ui/core/Slide";
-import Link from "next/link";
 
 function HideOnScroll(props) {
   const { children, window } = props;
@@ -34,7 +37,31 @@ HideOnScroll.propTypes = {
   window: PropTypes.func,
 };
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  icon: {
+    fill: "#c5198c",
+  },
+  wrapIcon: {
+    verticalAlign: "middle",
+    display: "inline-flex",
+  },
+  button: {
+    backgroundColor: "transparent",
+    "&:hover": {
+      backgroundColor: "transparent",
+      color: "#FFF",
+    },
+  },
+});
 export default function HideAppBar(props) {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -42,10 +69,10 @@ export default function HideAppBar(props) {
         <AppBar>
           <Toolbar>
             <Link href="/">
-              <Button>Workshops</Button>
+              <Button className={classes.button}>Workshops</Button>
             </Link>
             <Link href="workshops/add_workshop">
-              <Button>Add Workshop</Button>
+              <Button className={classes.button}>Add Workshop</Button>
             </Link>
           </Toolbar>
         </AppBar>
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,4 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { red } from "@material-ui/core/colors";
 
 // Create a theme instance.
 const theme = createMuiTheme({
@@ -11,7 +10,7 @@ const theme = createMuiTheme({
       main: "#19857b",
     },
     error: {
-      main: red.A400,
+      main: "#ff1744",
     },
     background: {
       default: "#fff",
